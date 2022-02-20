2aaa6c06427ff18a5e90968afb790f6e7707dd09
BOUCARON <julien.boucaron@gmail.com>
2022-02-20 10:31:55 +0100

    Create ControllerDialog class

--- software/3DMouseController.py
+++ software/3DMouseController.py
@@ -4,13 +4,20 @@ from PySide6 import QtCore, QtWidgets, QtGui
 from PySide6.QtWidgets import QDialog
 from ui_dialog import Ui_Dialog
 
+
+class ControllerDialog(QDialog):
+
+    def __init__(self, parent=None):
+        super(ControllerDialog, self).__init__(parent)
+        ui = Ui_Dialog();
+        ui.setupUi(self);
+
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
 
-    dialog = QDialog();
-    ui = Ui_Dialog();
-    ui.setupUi(dialog);
+    dialog = ControllerDialog();   
     dialog.show();
 
     sys.exit(app.exec())
